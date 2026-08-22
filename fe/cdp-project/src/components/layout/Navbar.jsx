70177c4e26ebf129901ddfa472da11625d95aea1
Hasibuan <abudujanasiregar@gmail.com>
2026-08-23 04:30:53 +0700

    feat(frontend): migrasi besar-besaran react javascript ke typescript murni

--- fe/cdp-project/src/components/layout/Navbar.jsx
+++
@@ -1,7 +0,0 @@
-export default function Navbar({ title }) {
-  return (
-    <header className="h-14 bg-white border-b border-slate-200 flex items-center pl-16 lg:pl-6 pr-6">
-      <h1 className="text-base font-semibold text-slate-800">{title}</h1>
-    </header>
-  );
-}
